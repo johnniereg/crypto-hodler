2b0047f23e1c160e96c6e16e523d2890043191bf
Johnnie Regalado <johnniereg@gmail.com>
2018-02-10 07:03:14 +0000

    Add holdings to portfolio object.
    Total amount in portfolio holdings is added as another key value pair to the portfolio. Single array being passed to the view for rendering.

--- app/controllers/holdings_controller.rb
+++ app/controllers/holdings_controller.rb
@@ -2,7 +2,7 @@ class HoldingsController < ApplicationController
 
     def index
         @holdings = Holding.all
-        puts build_portfolio
+        @portfolio = build_portfolio
     end
     
     def show
@@ -46,31 +46,61 @@ class HoldingsController < ApplicationController
         redirect_to holdings_path
     end
 
+    # Return CoinMarketCap API response for all coins
     def all_tokens
         all_tokens = HTTParty.get('https://api.coinmarketcap.com/v1/ticker/')
-        # JSON.parse all_tokens, symbolize_names: true
     end
 
+    # Assemble holdings and API request into a portfolio
     def build_portfolio
         @portfolio = Array.new
         @all_tokens = all_tokens
+        # @holdings = get_holdings
         @portfolio_names = portfolio_names
+        # @names_and_amounts = names_and_amounts(@holdings)
         @all_tokens.each do |token|
             if @portfolio_names.include? token["id"]
+                token["holding"] = get_amount(token["id"])
                 @portfolio.push(token)
             end
         end
-        @portfolio
+        puts @portfolio
     end
 
+    # Return the holding amount of a given token
+    def get_amount(name)
+        @holding = get_holding(name)
+        @amount = @holding["amount"]
+    end
+
+    # Return specific holding (name and amount)
+    def get_holding(name)
+        @holding = Holding.find_by crypto: name
+    end
+
+    # Return database holdings (names and amounts)
+    def get_holdings
+        @holdings = Holding.all
+    end
+
+    # Collect names of tokens held in the portfolio, used to carve down API results.
     def portfolio_names
         @portfolio_names = Array.new
-        @holdings = Holding.all
+        @holdings = get_holdings
         @holdings.each do |holding|
             @portfolio_names.push(holding.crypto)
         end
         @portfolio_names
     end
+
+    # def names_and_amounts(holdings)
+    #     @names_and_amounts = Array.new
+    #     @holdings = holdings
+    #     @holdings.each do |holding|
+    #         @portfolio_names.push({"name" => holding.crypto, "amount" => holding.amount})
+    #     end
+    #     @names_and_amounts
+    # end
 
     private
         def holding_params
